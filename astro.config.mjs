7e0a6a211810df9639ec288d18ce6271120c0a38
sarahk28 <sarahk2028@gmail.com>
2026-07-10 19:00:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-site: "https://{sarahk28}.github.io",
-base: "/{devportfolio}/",
+site: "https://sarahk28.github.io",
+base: "/devportfolio",
 vite: {
 plugins: [tailwindcss()],
 },
